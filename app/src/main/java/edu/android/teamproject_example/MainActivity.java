d58051f8c9450128d2e503c826714cf1194baa24
ku8230 <ku8230@naver.com>
2018-10-19 16:15:32 +0900

    김정완 textKim

--- app/src/main/java/edu/android/teamproject_example/MainActivity.java
+++ app/src/main/java/edu/android/teamproject_example/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
+    private TextView textKim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView);
 
         textView.setText("훈식아 PPT 만들어 // 최소한 선택권은 주세요....!!!");
+
+        textKim = findViewById(R.id.textKim);
+        textKim.setText("롤 똥쟁이 오훈식");
 
     }
 }
