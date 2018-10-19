38e9006ca35374b51f34016129148329c74eb3a0
Taewon Eum <twon143@naver.com>
2018-10-19 16:20:34 +0900

    textView2, button 추가 및 기능 구현

--- app/src/main/java/edu/android/teamproject_example/MainActivity.java
+++ app/src/main/java/edu/android/teamproject_example/MainActivity.java
@@ -2,10 +2,13 @@ package edu.android.teamproject_example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
+    private TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,7 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.textView);
 
-        textView.setText("훈식아 PPT 만들어 // 최소한 선택권은 주세요....!!!");
+        textView.setText("훈식아 PPT 만들어 // 최소한 선택권은 없음....!!!");
 
+        textView2 = findViewById(R.id.textView2);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView2.append("PPT 만들자 ");
+            }
+        });
     }
 }
