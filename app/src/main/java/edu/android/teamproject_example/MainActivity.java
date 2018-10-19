89c1725cdb943e3bc5a65a6d9d4f27b8f61e7bfb
pleete <qsdr92@nate.com>
2018-10-19 16:09:43 +0900

    textView 수정

--- app/src/main/java/edu/android/teamproject_example/MainActivity.java
+++ app/src/main/java/edu/android/teamproject_example/MainActivity.java
@@ -2,12 +2,17 @@ package edu.android.teamproject_example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        textView.setText("훈식아 PPT 만들어");
     }
 }
